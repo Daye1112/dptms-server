1ff359786964cf15200e8a3ca40bfba0111448b0
luyuhao <ludaye1112@163.com>
2020-07-19 22:25:51 +0800

    描述：初始化config模块 日期：2020-07-19

---
+++ dptms-config/src/main/java/com/darren1112/config/ConfigApplication.java
@@ -0,0 +1,24 @@
+package com.darren1112.config;
+
+import com.darren1112.common.util.EnvironmentAwareUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.config.ConfigFileApplicationListener;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author luyuhao
+ * @date 2020/07/19 22:09
+ */
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args){
+        EnvironmentAwareUtil.adjust();
+        System.setProperty(ConfigFileApplicationListener.ACTIVE_PROFILES_PROPERTY, "native");
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
