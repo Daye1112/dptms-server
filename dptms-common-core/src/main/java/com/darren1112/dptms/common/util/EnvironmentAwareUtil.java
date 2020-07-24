ad014ec54a22d954a30777acc2afb9073e405f7d
luyuhao <ludaye1112@163.com>
2020-07-25 01:26:52 +0800

    描述：密钥加载调整为读取环境变量 日期：2020-07-25

--- dptms-common-core/src/main/java/com/darren1112/dptms/common/util/EnvironmentAwareUtil.java
+++ dptms-common-core/src/main/java/com/darren1112/dptms/common/util/EnvironmentAwareUtil.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.config.ConfigFileApplicationListener;
 import org.springframework.util.StringUtils;
 
+import java.util.Optional;
+
 /**
  * @author luyuhao
  * @date 2019/12/4 18:38
@@ -11,13 +13,32 @@ import org.springframework.util.StringUtils;
 @Slf4j
 public class EnvironmentAwareUtil {
 
+    /**
+     * 环境变量：系统环境
+     */
+    private static final String ENV_LOCATION = "dptms";
+
+    /**
+     * 系统环境默认：local
+     */
+    private static final String DEFAULT_LOCATION = "local";
+
+    /**
+     * 环境变量：系统密钥
+     */
+    private static final String SECRET = "dptmsSecret";
+
+
     public static void adjust(){
-        String path = System.getenv("dptms");
-        if(StringUtils.isEmpty(path)){
-            path = "local";
+        String location = System.getenv(ENV_LOCATION);
+        String dptmsSecret = Optional.ofNullable(System.getenv(SECRET)).orElse("");
+        if(StringUtils.isEmpty(location)){
+            location = DEFAULT_LOCATION;
         }
-        log.info("当前环境为 {}" , path);
-        System.setProperty(ConfigFileApplicationListener.CONFIG_LOCATION_PROPERTY, "classpath:/config/" + path + "/");
-        System.setProperty(ConfigFileApplicationListener.ACTIVE_PROFILES_PROPERTY, path);
+        log.info("当前环境为 {}" , location);
+        log.info("系统密钥为 {}" , dptmsSecret);
+        System.setProperty(ConfigFileApplicationListener.CONFIG_LOCATION_PROPERTY, "classpath:/config/" + location + "/");
+        System.setProperty(ConfigFileApplicationListener.ACTIVE_PROFILES_PROPERTY, location);
+        System.setProperty("jasypt.encryptor.password", dptmsSecret);
     }
 }
