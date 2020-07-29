d0e12991d72ea514b9cf3cb9201f6e77cf20567e
luyuhao <ludaye1112@163.com>
2020-07-30 01:27:37 +0800

    描述：修改eureka相关配置 日期：2020-07-30

--- dptms-eureka/src/main/java/com/darren1112/dptms/eureka/config/WebSecurityConfig.java
+++ dptms-eureka/src/main/java/com/darren1112/dptms/eureka/config/WebSecurityConfig.java
@@ -1,8 +1,21 @@
 package com.darren1112.dptms.eureka.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
+ * security配置
+ *
  * @author luyuhao
  * @since 2020/7/29 21:06
  */
-public class WebSecurityConfig {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
 }
