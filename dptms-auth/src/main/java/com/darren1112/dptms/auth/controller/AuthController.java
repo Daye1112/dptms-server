74d211a77b21aa917793c32fc8cfd0660ba8eb19
luyuhao <ludaye1112@163.com>
2020-07-31 02:35:27 +0800

    描述：新增gateway配置 日期：2020-07-31

--- dptms-auth/src/main/java/com/darren1112/dptms/auth/controller/AuthController.java
+++ dptms-auth/src/main/java/com/darren1112/dptms/auth/controller/AuthController.java
@@ -1,10 +1,21 @@
 package com.darren1112.dptms.auth.controller;
 
 import com.darren1112.dptms.auth.service.SysUserService;
+import com.darren1112.dptms.common.exception.BadRequestException;
+import com.darren1112.dptms.common.message.JsonResult;
+import com.darren1112.dptms.common.util.ResponseEntityUtil;
+import com.darren1112.dptms.common.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 /**
  * 权限相关Controller
@@ -20,4 +31,26 @@ public class AuthController {
     @Autowired
     private SysUserService sysUserService;
 
+    @Autowired
+    private ConsumerTokenServices consumerTokenServices;
+
+    @GetMapping("oauth/test")
+    public String testOauth() {
+        return "oauth";
+    }
+
+    @GetMapping("user")
+    public Principal currentUser(Principal principal) {
+        return principal;
+    }
+
+    @DeleteMapping("signOut")
+    public ResponseEntity signOut(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+        String token = StringUtil.replace(authorization, "bearer ", "");
+        if (!consumerTokenServices.revokeToken(token)) {
+            throw new BadRequestException("退出登录失败");
+        }
+        return ResponseEntityUtil.ok(JsonResult.buildMsg("退出登录成功"));
+    }
 }
---
+++ dptms-gateway/src/main/java/com/darren1112/dptms/gateway/GatewayApplication.java
@@ -0,0 +1,28 @@
+package com.darren1112.dptms.gateway;
+
+import com.darren1112.dptms.common.util.EnvironmentAwareUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 启动类
+ *
+ * @author luyuhao
+ * @date 2020/07/31 02:12
+ */
+@EnableZuulProxy
+@EnableCaching
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = {
+        "com.darren1112.dptms.gateway",
+        "com.darren1112.dptms.common.component"})
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        EnvironmentAwareUtil.adjust();
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
