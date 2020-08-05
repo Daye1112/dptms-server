57c2f10fc8af0659cf25bf648c4133d07492b4f8
luyuhao <ludaye1112@163.com>
2020-08-06 02:38:05 +0800

    描述：初始化doc-starter模块 日期：2020-08-06

---
+++ dptms-common/dptms-common-doc-starter/src/main/java/com/darren1112/dptms/common/doc/starter/config/DocAutoConfig.java
@@ -0,0 +1,10 @@
+package com.darren1112.dptms.common.doc.starter.config;
+
+/**
+ * 文档自动配置类
+ *
+ * @author luyuhao
+ * @date 2020/08/06 02:36
+ */
+public class DocAutoConfig {
+}
---
+++ dptms-common/dptms-common-doc-starter/src/main/java/com/darren1112/dptms/common/doc/starter/properties/DocProperties.java
@@ -0,0 +1,10 @@
+package com.darren1112.dptms.common.doc.starter.properties;
+
+/**
+ * 文档信息配置
+ *
+ * @author luyuhao
+ * @date 2020/08/06 02:37
+ */
+public class DocProperties {
+}
