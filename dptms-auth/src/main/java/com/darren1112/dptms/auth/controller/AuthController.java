b744ea26e51f651b3d30b7fe52ed486aacae4eea
luyuhao <ludaye1112@163.com>
2020-08-09 04:10:34 +0800

    描述：knife4j版本升级 日期：2020-08-09

--- dptms-auth/src/main/java/com/darren1112/dptms/auth/controller/AuthController.java
+++ dptms-auth/src/main/java/com/darren1112/dptms/auth/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.darren1112.dptms.auth.controller;
 
-import com.darren1112.dptms.auth.service.SysUserService;
 import com.darren1112.dptms.common.core.exception.BadRequestException;
 import com.darren1112.dptms.common.core.message.JsonResult;
 import com.darren1112.dptms.common.core.util.ResponseEntityUtil;
@@ -11,7 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
@@ -29,24 +31,49 @@ import java.security.Principal;
 public class AuthController {
 
     @Autowired
-    private SysUserService sysUserService;
-
-    @Autowired
     private ConsumerTokenServices consumerTokenServices;
 
-    @ApiOperation(value = "测试接口")
-    @GetMapping("/test")
-    public String testOauth(String username, String password, String grant_type) {
-        return "forward:/oauth/token";
+    /**
+     * 认证接口-测试使用
+     *
+     * @param username   用户名
+     * @param password   密码
+     * @param grant_type 访问类型
+     * @return null
+     * @author luyuhao
+     * @date 20/08/09 03:28
+     */
+    @ApiOperation(value = "认证接口-测试使用", notes = "将test改为token，进行认证授权，获取token，header：Basic ZHB0bXM6MTIzNDU2")
+    @PostMapping("/oauth/test")
+    public ResponseEntity<JsonResult> testOauth(String username, String password, String grant_type) {
+        return ResponseEntityUtil.ok(JsonResult.buildData(null));
     }
 
+    /**
+     * 获取当前用户信息
+     *
+     * @param principal security用户对象
+     * @return Principal
+     * @author luyuhao
+     * @date 20/08/09 03:29
+     */
+    @ApiOperation(value = "获取当前用户信息")
     @GetMapping("/user")
     public Principal currentUser(Principal principal) {
         return principal;
     }
 
-    @DeleteMapping("signOut")
-    public ResponseEntity signOut(HttpServletRequest request) {
+    /**
+     * 注销
+     *
+     * @param request 请求域
+     * @return JsonResult
+     * @author luyuhao
+     * @date 20/08/09 03:34
+     */
+    @ApiOperation(value = "注销")
+    @GetMapping("signOut")
+    public ResponseEntity<JsonResult> signOut(HttpServletRequest request) {
         String authorization = request.getHeader("Authorization");
         String token = StringUtil.replace(authorization, "bearer ", "");
         if (!consumerTokenServices.revokeToken(token)) {
--- dptms-system-manage/src/main/java/com/darren1112/dptms/system/sys/controller/SysUserController.java
+++ dptms-system-manage/src/main/java/com/darren1112/dptms/system/sys/controller/SysUserController.java
@@ -1,9 +1,13 @@
 package com.darren1112.dptms.system.sys.controller;
 
-import com.darren1112.dptms.common.core.exception.BadRequestException;
+import com.darren1112.dptms.common.core.message.JsonResult;
+import com.darren1112.dptms.common.core.util.ResponseEntityUtil;
 import com.darren1112.dptms.system.sys.service.SysUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import java.security.Principal;
  * @date 2020/07/25 02:14
  */
 @Slf4j
+@Api(tags = "用户管理")
 @RestController
 @RequestMapping(value = "/sys/user")
 public class SysUserController {
@@ -24,13 +29,17 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
-    @GetMapping("/info")
-    public String test(){
-        return "dptms-system-manage";
-    }
-
+    /**
+     * 获取用户信息
+     *
+     * @param principal security用户对象
+     * @return Principal
+     * @author luyuhao
+     * @date 20/08/09 03:36
+     */
+    @ApiOperation("获取用户信息")
     @GetMapping("/user")
-    public Principal currentUser(Principal principal) {
-        return principal;
+    public ResponseEntity<JsonResult<Principal>> currentUser(Principal principal) {
+        return ResponseEntityUtil.ok(JsonResult.buildData(principal));
     }
 }
