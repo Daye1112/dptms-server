16592efccc14b21e4d40dc4f13caf66718d628e3
luyuhao <ludaye1112@163.com>
2020-08-11 01:19:48 +0800

    描述：完成Jasypt加密工具类 日期：2020-08-11

---
+++ dptms-common/dptms-common-core/src/test/java/com/darren1112/dptms/test/JasyptTest.java
@@ -0,0 +1,19 @@
+package com.darren1112.dptms.test;
+
+import com.darren1112.dptms.common.core.util.JasyptUtil;
+import org.junit.Test;
+
+/**
+ * @author luyuhao
+ * @date 2020/07/22 01:22
+ */
+public class JasyptTest {
+
+    @Test
+    public void test01() {
+        String secret = "xxx";
+        String str1 = "yyy";
+
+        System.out.println(str1 + ": " + JasyptUtil.defaultEncrypt(str1, secret));
+    }
+}
