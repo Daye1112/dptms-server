d8f8aa6b5fcca85da3ad2341b126e478c5c1e2e6
luyuhao <ludaye1112@163.com>
2020-11-29 23:03:32 +0800

    描述：开发权限鉴权功能 日期：2020-11-29

--- dptms-gateway/src/main/java/com/darren1112/dptms/gateway/common/filter/PermissionValidateFilter.java
+++ dptms-gateway/src/main/java/com/darren1112/dptms/gateway/common/filter/PermissionValidateFilter.java
@@ -1,7 +1,12 @@
 package com.darren1112.dptms.gateway.common.filter;
 
 import com.darren1112.dptms.common.core.constants.FileConstant;
-import com.darren1112.dptms.common.core.util.UrlUtil;
+import com.darren1112.dptms.common.core.constants.SecurityConstant;
+import com.darren1112.dptms.common.core.message.JsonResult;
+import com.darren1112.dptms.common.core.util.*;
+import com.darren1112.dptms.common.redis.starter.util.TokenUtil;
+import com.darren1112.dptms.common.spi.common.entity.ActiveUser;
+import com.darren1112.dptms.gateway.common.enums.GatewayErrorCodeEnum;
 import com.darren1112.dptms.gateway.common.properties.SecurityProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +18,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * 权限过滤器
@@ -26,6 +32,9 @@ public class PermissionValidateFilter  extends OncePerRequestFilter {
 
     @Autowired
     private SecurityProperties securityProperties;
+
+    @Autowired
+    private TokenUtil tokenUtil;
 
     /**
      * 是否需要过滤
@@ -48,10 +57,20 @@ public class PermissionValidateFilter  extends OncePerRequestFilter {
             chain.doFilter(request, response);
             return;
         }
-        // TODO 查询用户信息
-        // 判断是否更新过密码
-        // 判断是否锁定
-        // 查询权限list
+        // token校验
+        // 优先处理cookie中携带的cookie
+        String refreshToken = CookieUtil.getCookie(SecurityConstant.REFRESH_TOKEN_KEY, request);
+        if (StringUtil.isBlank(refreshToken)) {
+            refreshToken = request.getHeader(SecurityConstant.REFRESH_TOKEN_KEY);
+        }
+        // 获取redis中的用户信息
+        ActiveUser activeUser = tokenUtil.getActiveUser(refreshToken);
+        if(Objects.isNull(activeUser)){
+            ResponseUtil.setJsonResult(response, JsonResult.buildErrorEnum(GatewayErrorCodeEnum.NOT_LOGIN));
+            return;
+        }
+        // TODO 【用户权限功能完成后开发】查询权限list
+
         // 判断uri是否在权限list中
         chain.doFilter(request, response);
     }
--- dptms-system-manage/src/main/java/com/darren1112/dptms/system/sys/controller/SysUserController.java
+++ dptms-system-manage/src/main/java/com/darren1112/dptms/system/sys/controller/SysUserController.java
@@ -1,18 +1,11 @@
 package com.darren1112.dptms.system.sys.controller;
 
-import com.darren1112.dptms.common.core.message.JsonResult;
-import com.darren1112.dptms.common.core.util.ResponseEntityUtil;
 import com.darren1112.dptms.system.sys.service.SysUserService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 /**
  * 系统用户Controller
@@ -29,17 +22,4 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
-    /**
-     * 获取用户信息
-     *
-     * @param principal security用户对象
-     * @return Principal
-     * @author luyuhao
-     * @date 20/08/09 03:36
-     */
-    @ApiOperation("获取用户信息")
-    @GetMapping("/user")
-    public ResponseEntity<JsonResult<Principal>> currentUser(Principal principal) {
-        return ResponseEntityUtil.ok(JsonResult.buildSuccessData(principal));
-    }
 }
