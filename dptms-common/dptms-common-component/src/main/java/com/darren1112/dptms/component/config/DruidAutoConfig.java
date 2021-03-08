9843067d3473f7b1d04c6d85416f70e59ab13c24
luyuhao <ludaye1112@163.com>
2021-03-08 17:24:57 +0800

    描述：组件优化 日期：2021-03-08

--- dptms-common/dptms-common-component/src/main/java/com/darren1112/dptms/component/config/DruidAutoConfig.java
+++ dptms-common/dptms-common-component/src/main/java/com/darren1112/dptms/component/config/DruidAutoConfig.java
@@ -3,7 +3,6 @@ package com.darren1112.dptms.component.config;
 import com.darren1112.dptms.component.controller.DruidStatController;
 import com.darren1112.dptms.component.service.DruidService;
 import com.darren1112.dptms.component.service.impl.DruidServiceImpl;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.Bean;
  * @author luyuhao
  * @date 2021/02/15 21:55
  */
-@EnableFeignClients(basePackages = {"com.darren1112.dptms"})
 public class DruidAutoConfig {
 
     @Bean
