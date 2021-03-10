3f654cfa156b4aa5add6051986c5ab04532c0628
luyuhao <ludaye1112@163.com>
2021-03-11 02:09:58 +0800

    描述：新增配置表 日期：2020-03-11

---
+++ 文档/生产发布/1.0.0/数据库脚本/service_config_profile.sql
@@ -0,0 +1,15 @@
+CREATE TABLE service_config_profile
+(
+  id             BIGINT NOT NULL AUTO_INCREMENT COMMENT 'id',
+  application_id bigint COMMENT '应用id 关联service_application.id',
+  profile_code   varchar(64) COMMENT '环境编号',
+  profile_name   varchar(64) COMMENT '环境名称',
+  remark         VARCHAR(256) COMMENT '备注',
+  isvalid        TINYINT COMMENT '是否有效',
+  ctime          DATETIME COMMENT '创建时间',
+  creater        BIGINT COMMENT '创建者',
+  mtime          DATETIME COMMENT '更新时间',
+  updater        BIGINT COMMENT '更新者',
+  PRIMARY KEY (id)
+) COMMENT ='配置环境表';
+
