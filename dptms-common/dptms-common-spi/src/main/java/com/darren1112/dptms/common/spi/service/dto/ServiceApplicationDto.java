4bb67016b7a0685d6257b5843aa4349876cc139a
luyuhao <ludaye1112@163.com>
2021-03-21 21:26:34 +0800

    描述：代码优化 日期：2020-03-21

--- dptms-common/dptms-common-spi/src/main/java/com/darren1112/dptms/common/spi/service/dto/ServiceApplicationDto.java
+++ dptms-common/dptms-common-spi/src/main/java/com/darren1112/dptms/common/spi/service/dto/ServiceApplicationDto.java
@@ -1,6 +1,7 @@
 package com.darren1112.dptms.common.spi.service.dto;
 
 import com.darren1112.dptms.common.spi.service.entity.ServiceApplicationEntity;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -20,4 +21,10 @@ public class ServiceApplicationDto extends ServiceApplicationEntity {
      * 是否更新 true/false
      */
     private Boolean isUpdate;
+
+    /**
+     * 查询key
+     */
+    @ApiModelProperty(value = "查询key")
+    private String researchKey;
 }
