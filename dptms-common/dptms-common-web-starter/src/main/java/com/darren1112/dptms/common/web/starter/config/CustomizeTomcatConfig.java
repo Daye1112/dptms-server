f1307797445c99be3adb2d12e5ae427669686f7d
luyuhao <ludaye1112@163.com>
2021-05-10 09:27:39 +0800

    描述：添加tomcat配置 日期：2021-05-10

---
+++ dptms-common/dptms-common-web-starter/src/main/java/com/darren1112/dptms/common/web/starter/config/CustomizeTomcatConfig.java
@@ -0,0 +1,28 @@
+package com.darren1112.dptms.common.web.starter.config;
+
+import com.darren1112.dptms.common.web.starter.properties.TomcatConnectorProperties;
+import com.darren1112.dptms.common.web.starter.tomcat.DptmsTomcatConnectorCustomizer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * tomcat配置
+ *
+ * @author luyuhao
+ * @since 2021/5/10 9:13
+ */
+@Slf4j
+@EnableConfigurationProperties(TomcatConnectorProperties.class)
+public class CustomizeTomcatConfig {
+
+    @Autowired
+    private TomcatConnectorProperties tomcatConnectorProperties;
+
+    @Bean
+    public DptmsTomcatConnectorCustomizer dptmsTomcatConnectorCustomizer() {
+        return new DptmsTomcatConnectorCustomizer(tomcatConnectorProperties);
+    }
+
+}
---
+++ dptms-common/dptms-common-web-starter/src/main/java/com/darren1112/dptms/common/web/starter/tomcat/DptmsTomcatConnectorCustomizer.java
@@ -0,0 +1,50 @@
+package com.darren1112.dptms.common.web.starter.tomcat;
+
+import com.darren1112.dptms.common.web.starter.properties.TomcatConnectorProperties;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ProtocolHandler;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+
+/**
+ * 自定义tomcat容器的连接器配置
+ *
+ * @author luyuhao
+ * @since 2021/5/10 9:17
+ */
+public class DptmsTomcatConnectorCustomizer implements TomcatConnectorCustomizer {
+
+    private TomcatConnectorProperties tomcatConnectorProperties;
+
+    public DptmsTomcatConnectorCustomizer(TomcatConnectorProperties tomcatConnectorProperties) {
+        this.tomcatConnectorProperties = tomcatConnectorProperties;
+    }
+
+    @Override
+    public void customize(Connector connector) {
+        if (null == connector) {
+            return;
+        }
+        connector.setURIEncoding(tomcatConnectorProperties.getUriEncoding());
+        connector.setUseBodyEncodingForURI(tomcatConnectorProperties.isUseBodyEncodingForUri());
+        connector.setEnableLookups(tomcatConnectorProperties.isEnableLookups());
+        if (null == connector.getProtocolHandler()) {
+            return;
+        }
+        ProtocolHandler handler = connector.getProtocolHandler();
+        if (handler instanceof Http11NioProtocol) {
+            Http11NioProtocol protocol = (Http11NioProtocol) handler;
+            protocol.setCompression(tomcatConnectorProperties.getCompression());
+            protocol.setCompressionMinSize(tomcatConnectorProperties.getCompressionMinSize());
+            protocol.setNoCompressionUserAgents(tomcatConnectorProperties.getNoCompressionUserAgents());
+            protocol.setCompressibleMimeType(tomcatConnectorProperties.getCompressibleMimeType());
+            protocol.setConnectionTimeout(tomcatConnectorProperties.getConnectionTimeOut());
+            protocol.setMaxThreads(tomcatConnectorProperties.getMaxThreads());
+            protocol.setMinSpareThreads(tomcatConnectorProperties.getMinSpareThreads());
+            protocol.setKeepAliveTimeout(tomcatConnectorProperties.getKeepAliveTimeOut());
+            protocol.setMaxKeepAliveRequests(tomcatConnectorProperties.getMaxKeepAliveRequests());
+            protocol.setAcceptCount(tomcatConnectorProperties.getAcceptCount());
+            protocol.setDisableUploadTimeout(tomcatConnectorProperties.isDisableUploadTimeOut());
+        }
+    }
+}
